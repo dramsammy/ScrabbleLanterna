86f2347b32dd7c3be5564f4671468dea9d0c7962
Oliver Ripps <oripps@stuy.edu>
2019-01-03 18:44:34 -0500

    getcolor drafted and constructor modified to include played

--- Pieces.java
+++ Pieces.java
@@ -2,14 +2,19 @@ public class Pieces{
   int Score;
   char Letter;
   String Color;
-  public Pieces(char Lettervalue,int Scorevalue){
+  boolean Played;
+  public Pieces(char Lettervalue,int Scorevalue, String Colorimput, boolean beenplayed ){
     Letter=Lettervalue;
     Score=Scorevalue;
+    Color=Colorimput;
+    Played=beenplayed;
   }
   public String toString(){
     return "" + Letter;
   }
-
+  public String getColor(){
+    return Color;
+  }
 
 
 
