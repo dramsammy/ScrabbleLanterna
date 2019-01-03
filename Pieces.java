6b749499d5b0caab0cd7fbc5c3d2c81ae9a59883
Oliver Ripps <oripps@stuy.edu>
2019-01-03 18:45:34 -0500

    added wasplayed function

--- Pieces.java
+++ Pieces.java
@@ -3,17 +3,20 @@ public class Pieces{
   char Letter;
   String Color;
   boolean Played;
-  public Pieces(char Lettervalue,int Scorevalue, String Colorimput, boolean beenplayed ){
+  public Pieces(char Lettervalue,int Scorevalue, String Colorimput){
     Letter=Lettervalue;
     Score=Scorevalue;
     Color=Colorimput;
-    Played=beenplayed;
+    Played=false;
   }
   public String toString(){
     return "" + Letter;
   }
   public String getColor(){
     return Color;
+  }
+  public boolean wasplayed(){
+    return Played;
   }
 
 
