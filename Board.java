cb8cdf5ae032fbeabcd9392c317afbd4866caa64
dramsammy <dramsammy@stuy.edu>
2019-01-03 19:58:02 -0500

    changed up constructor and added addPiecesList in order to prepare for having pieces randomly sorted in arrays

--- Board.java
+++ Board.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 public class Board extends Pieces{
   int size;
   ArrayList<Pieces> pieces = new ArrayList<Pieces>();
+  Pieces[][] ary;
   // Constructor for Board takes in an integer and 2D array of pieces
-  public Board(int s, ArrayList<Pieces> p){
+  public Board(int s, ArrayList<Pieces> p, Pieces[][] a){
     size = s;
     pieces = p;
+    ary = a;
   }
   // Added clear method for a future restart method to be added in the game
   public void clear(){
@@ -16,6 +18,7 @@ public class Board extends Pieces{
   public int getSize(){
     return size;
   }
-  public void addPieces(Pieces p){
-
+  // Method to add pieces to array list in order to prepare to have pieces sorted randomly in Array
+  public void addPiecesList(Pieces p){
+    pieces.add(p);
   }
