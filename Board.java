64524c55ccdfd7ac477783fc981c06aa3c44abfa
Oliver Ripps <oripps@stuy.edu>
2019-01-04 11:46:48 -0500

    toString not working...end of class 1/4

--- Board.java
+++ Board.java
@@ -12,8 +12,8 @@ public class Board extends Pieces{
     board = a;
   }
   public static void main(String args[]){
-    ArrayList<Pieces> p = new ArrayList<Pieces>();
-    ArrayList<ArrayList<Pieces>> a = new ArrayList<ArrayList<Pieces>>();
+    ArrayList<Pieces> p = new ArrayList<Pieces>(15);
+    ArrayList<ArrayList<Pieces>> a = new ArrayList<ArrayList<Pieces>>(15);
     Board test = new Board(8, 8, p, a);
     System.out.println(test);
   }
@@ -49,16 +49,17 @@ public class Board extends Pieces{
   }*/
   // toString method that will display board as a 2D ArrayList of pieces
   public String toString(){
-    String toString = "---------------";
+    String str = "---------------";
     for (int i = 0; i < board.size(); i++){
-      toString+="| ";
+      str+="| ";
       for(int a = 0; a < board.get(i).size(); a++){
-        toString += board.get(i).get(a).toString();
+        str += (board.get(i).get(a)).toString();
+        System.out.println( (board.get(i).get(a)).toString());
       }
 
-      toString += " |\n";
+      str += " |\n";
     }
-    toString += "---------------";
-    return toString;
+    str += "---------------";
+    return str;
   }
 }
