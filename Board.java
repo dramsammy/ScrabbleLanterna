389ea17f5ffba94a088db9e48eb9ffe9e1e1d419
dramsammy <dramsammy@stuy.edu>
2019-01-03 20:54:44 -0500

    added clear method and tweaked constructors and arrays to have arraylists

--- Board.java
+++ Board.java
@@ -3,24 +3,37 @@ public class Board extends Pieces{
   int rows;
   int columns;
   ArrayList<Pieces> pieces = new ArrayList<Pieces>();
-  ArrayList<ArrayList<Pieces>>
-  // Constructor for Board takes in an integer and 2D array of pieces
-  public Board(int r, int c, ArrayList<Pieces> p, Pieces[][] a){
+  ArrayList<ArrayList<Pieces>> board = new ArrayList<ArrayList<Pieces>>;
+  // Constructor for Board takes in an integer and 2D ArrayList of pieces
+  public Board(int r, int c, ArrayList<Pieces> p, ArrayList<ArrayList<Pieces>> a){
     rows = r;
     columns = c;
     pieces = p;
-    ary = a;
+    board = a;
   }
   // Added clear method for a future restart method to be added in the game
   public void clear(){
-    size = null;
-    Pieces = new pieces[][];
+    board.removeAll();
   }
-  // Accessor method for size
-  public int getSize(){
-    return size;
+  // Accessor method for rows
+  public int getRows(){
+    return rows;
   }
-  // Method to add pieces to array list in order to prepare to have pieces sorted randomly in Array
+  // Accessor method for columns
+  public int getColumns(){
+    return columns;
+  }
+  // Method to add pieces to array list in order to prepare to have pieces sorted randomly in ArrayList
   public void addPiecesList(Pieces p){
     pieces.add(p);
   }
+  // Method that will initialize the 2D ArrayList and make it a board of pieces that are blank
+  public void addBlanks(){
+    for (int i = 0; i < rows; i++){
+      board.add(new ArrayList());
+      for (int a = 0; a < columns; a++){
+        board.get(i).add(Pieces(' ', 0, "white"));
+      }
+    }
+  }
+}
