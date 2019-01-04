93ecad144f5da73d67935bc240e34e6252c0fd7a
dramsammy <dramsammy@stuy.edu>
2019-01-03 19:46:02 -0500

    finished constructor for Board

--- Board.java
+++ Board.java
@@ -1,4 +1,9 @@
 public class Board extends Pieces{
   int size;
   Pieces pieces[][];
+  // Constructor for Board takes in an integer and 2D array of pieces
+  public Board(int s, Pieces p[][]){
+    size = s;
+    pieces = p;
+  }
 }
