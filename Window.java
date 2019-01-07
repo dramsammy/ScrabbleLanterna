f2e3523509aa4a5b975adfe922ce57689ed69070
dramsammy <dramsammy@stuy.edu>
2019-01-07 00:32:40 -0500

    worked on the jframe a lot more almost done with printing the game

--- Window.java
+++ Window.java
@@ -6,8 +6,11 @@ public class Window extends JFrame{
   private JFrame Scrabble = new JFrame("Scrabble");
   private JPanel GameMenu = new JPanel();
   private JButton StartGame = new JButton("Start Game");
+  private JTextField textField = new JTextField();
   private Pieces[][] board= new Pieces[15][15];
   private Board test = new Board(8, 8, board);
+  private JLabel game = new JLabel();
+  Graphics g;
   public Window(){
     Start();
 
@@ -20,19 +23,32 @@ public class Window extends JFrame{
     Scrabble.setVisible(true);
     Scrabble.setDefaultCloseOperation(Scrabble.EXIT_ON_CLOSE);
     GameMenu.setBackground(Color.YELLOW);
+    GameMenu.setLayout(new GridLayout(9,9));
     Begin();
     GameMenu.add(StartGame);
 
-    Scrabble.add(GameMenu, BorderLayout.SOUTH);
+    Scrabble.add(GameMenu);
   }
+
+
   public void Begin(){
     StartGame.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent event){
-        System.out.println(test);
+        StartGame.setVisible(false);
+        GameMenu.setBackground(Color.WHITE);
+        Pieces r = new Pieces(' ',0);//whatever letter you put in the '' will fill the whole board
+        for (int i = 0; i < board.length; i++){
+          for(int l = 0; l < board[i].length; l++){
+            board[i][l]=r;
+          }
+        }
+        game.setText(test.toString());
+        GameMenu.add(game);
       }
     }
     );
   }
+
 
   public static void main(String[] args) {
     new Window();
