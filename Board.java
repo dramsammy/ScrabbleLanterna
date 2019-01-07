d0de2ea5dc54a5717a7a3204533247a216fbbf8e
Oliver Ripps <oripps@stuy.edu>
2019-01-07 18:36:04 -0500

    added fucntions to game with the peice numbers in the game

--- Board.java
+++ Board.java
@@ -85,7 +85,7 @@ public class Board extends Pieces{
     }
 
 
-  public static void main(String args[]){
+  /*public static void main(String args[]){
     Pieces[][] a =new Pieces[15][15];
     Pieces r = new Pieces(' ',0);//whatever letter you put in the '' will fill the whole board
     for (int i = 0; i < a.length; i++){
@@ -95,15 +95,15 @@ public class Board extends Pieces{
     }
     Board test = new Board(8, 8, a);
     System.out.println(test);
-  }
+  }*/
   // Added clear method for a future restart method to be added in the game
-  public void clear(){
+  /*public void clear(){
     for(int i=0;i<board.length;i++){
       for(int l=0;l<board[i].length;l++){
         board[i][l]=null;
     }
   }
-}
+}*/
 
   // Accessor method for rows
 /*  public int getRows(){
