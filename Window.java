122c3b49fd42cdb265b82810732611ec80616607
dramsammy <dramsammy@stuy.edu>
2019-01-06 21:36:23 -0500

    working towards making the jframe to call functions

--- Window.java
+++ Window.java
@@ -1,8 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-public class Window{
+
+public class Window extends JFrame{
+  private JFrame Scrabble = new JFrame("Scrabble");
+  private JPanel GameMenu = new JPanel();
+  private JButton StartGame = new JButton("Start Game");
   public static void main(String[] args) {
-      JFrame Scrabble = new JFrame();
       Scrabble.setSize(500,500);
+      Scrabble.setLocationRelativeTo(null);
       Scrabble.setVisible(true);
+      Scrabble.setDefaultCloseOperation(Scrabble.EXIT_ON_CLOSE);
+      GameMenu.setBackground(Color.YELLOW);
+
+      GameMenu.add(StartGame);
+
+      Scrabble.add(GameMenu, BorderLayout.SOUTH);
   }
 }
