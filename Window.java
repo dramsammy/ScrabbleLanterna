478481d25bc75eca752f724127c178b7a8b2023b
dramsammy <dramsammy@stuy.edu>
2019-01-06 21:45:48 -0500

    cleaned up methods

--- Window.java
+++ Window.java
@@ -6,15 +6,27 @@ public class Window extends JFrame{
   private JFrame Scrabble = new JFrame("Scrabble");
   private JPanel GameMenu = new JPanel();
   private JButton StartGame = new JButton("Start Game");
+
+  public Window(){
+    Start();
+  }
+
+
+  public void Start(){
+    Scrabble.setSize(500,500);
+    Scrabble.setLocationRelativeTo(null);
+    Scrabble.setVisible(true);
+    Scrabble.setDefaultCloseOperation(Scrabble.EXIT_ON_CLOSE);
+    GameMenu.setBackground(Color.YELLOW);
+
+    GameMenu.add(StartGame);
+
+    Scrabble.add(GameMenu, BorderLayout.SOUTH);
+  }
+
+  
   public static void main(String[] args) {
-      Scrabble.setSize(500,500);
-      Scrabble.setLocationRelativeTo(null);
-      Scrabble.setVisible(true);
-      Scrabble.setDefaultCloseOperation(Scrabble.EXIT_ON_CLOSE);
-      GameMenu.setBackground(Color.YELLOW);
+    new Window();
 
-      GameMenu.add(StartGame);
-
-      Scrabble.add(GameMenu, BorderLayout.SOUTH);
   }
 }
