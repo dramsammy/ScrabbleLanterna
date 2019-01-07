83de41a07f46f18ac6bea0518357184de0ee9e1d
dramsammy <dramsammy@stuy.edu>
2019-01-06 23:25:40 -0500

    got some of the jframe to start working

--- Window.java
+++ Window.java
@@ -6,9 +6,11 @@ public class Window extends JFrame{
   private JFrame Scrabble = new JFrame("Scrabble");
   private JPanel GameMenu = new JPanel();
   private JButton StartGame = new JButton("Start Game");
-
+  private Pieces[][] board= new Pieces[15][15];
+  private Board test = new Board(8, 8, board);
   public Window(){
     Start();
+
   }
 
 
@@ -18,13 +20,20 @@ public class Window extends JFrame{
     Scrabble.setVisible(true);
     Scrabble.setDefaultCloseOperation(Scrabble.EXIT_ON_CLOSE);
     GameMenu.setBackground(Color.YELLOW);
-
+    Begin();
     GameMenu.add(StartGame);
 
     Scrabble.add(GameMenu, BorderLayout.SOUTH);
   }
+  public void Begin(){
+    StartGame.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent event){
+        System.out.println(test);
+      }
+    }
+    );
+  }
 
-  
   public static void main(String[] args) {
     new Window();
 
