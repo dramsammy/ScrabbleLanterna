44adb68c590466469035a8222c1ddc3f1811fd4c
dramsammy <dramsammy@stuy.edu>
2019-01-06 21:00:43 -0500

    started jframe gui for our scrabble

--- Window.java
+++ Window.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
 public class Window{
-  
+  public static void main(String[] args) {
+      JFrame Scrabble = new JFrame();
+      Scrabble.setSize(500,500);
+      Scrabble.setVisible(true);
+  }
 }
