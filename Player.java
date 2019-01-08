abf2532a9151d2e93c3f660040381ba7686dc2c1
dramsammy <dramsammy@stuy.edu>
2019-01-07 19:37:01 -0500

    fixed dictionary

--- Player.java
+++ Player.java
@@ -5,7 +5,7 @@ public class Player{
   private Pieces[] theHand = new Pieces[7];
   private Pieces[] toBePlayed;
   private Board Game;
-  private char[] randomLetters = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+  private char[] randomLetters = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
   private int[] selection = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
   private Random hand = new Random();
   public Player(Board b){
