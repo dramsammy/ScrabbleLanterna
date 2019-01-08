76cc9a5c647003bc35aa38fcde0cb64313206ce9
dramsammy <dramsammy@stuy.edu>
2019-01-07 19:44:14 -0500

    fixed some instance variables

--- Player.java
+++ Player.java
@@ -7,6 +7,7 @@ public class Player{
   private Board Game;
   private char[] randomLetters = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
   private int[] selection = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+  private int[] played;
   private Random hand = new Random();
   public Player(Board b){
     b = Game;
@@ -20,9 +21,9 @@ public class Player{
   }
   //Function which takes in an input of the values selected of each piece and sets an array to be played
   public Piece[] startTurn(){
-    for (int i = 0; i < selection.length; i ++){
-      toBePlayed = new Pieces[selection];
-      toBePlayed[i] = randomLetters[i];
+    for (int i = 0; i < played.length; i ++){
+      toBePlayed = new Pieces[played.length];
+      toBePlayed[i] = randomLetters[selection[i]];
     }
   }
   public int setScoreforLetter(char L){
@@ -49,5 +50,8 @@ public class Player{
       Score=10;
     }
     return Score;
+  }
+  public boolean isValidWord(Piece[] p){
+    p
   }
 }
