ffb5267f1a3278a6a359c48640b5a9db0181005d
dramsammy <dramsammy@stuy.edu>
2019-01-08 20:41:42 -0500

    finished instance variables for game

--- Game.java
+++ Game.java
@@ -1,67 +1,68 @@
 import java.util.ArrayList;
 public class Game {
   private int Players;
-  private ArrayList<char> pile= new ArrayList<int>();
-  private ArrayList<char> hand1=new ArrayList<int>();
-  private ArrayList<char> hand2=new ArrayList<int>();
-  private ArrayList<char> hand3=new ArrayList<int>();
-  private ArrayList<char> hand4=new ArrayList<int>();
-  public Game(int p){
-    Players = p;
-    ArrayList<char> p =new ArrayList<char>();
-    p.add('X');
-    p.add('Z');
-    p.add('Q');
-    p.add('K');
-    p.add('J');
-    for(int i=0;i<2;i++){
-      p.add('Y');
-      p.add('W');
-      p.add('V');
-      p.add('P');
-      p.add('M');
-      p.add('H');
-      p.add('F');
-      p.add('C');
-      p.add('B');
-    }
-    for(int i=0;i<3;i++){
-      p.add('G');
-    }
-    for(int i=0;i<4;i++){
-      p.add('U');
-      p.add('S');
-      p.add('L');
-      p.add('D');
-    }
-    for(int i=0;i<2;i++){
-      p.add('Y');
-      p.add('W');
-      p.add('V');
-      p.add('P');
-      p.add('M');
-      p.add('H');
-      p.add('F');
-      p.add('C');
-      p.add('B');
-    }
-    for(int i=0;i<6;i++){
-      p.add('T');
-      p.add('R');
-      p.add('N');
-    }
-    for(int i=0;i<8;i++){
-      p.add('O');
-    }
-    for(int i=0;i<9;i++){
-      p.add('I');
-      p.add('A');
-    }
-    for(int i=0;i<12;i++){
-      p.add('E');
-    }
-    //RANDOMLY GENERATE PEICES FOR EACH HAND DEPENDING ON P
-  }
+  private Board gameBoard;
+  // private ArrayList<char> pile= new ArrayList<int>();
+  // private ArrayList<char> hand1=new ArrayList<int>();
+  // private ArrayList<char> hand2=new ArrayList<int>();
+  // private ArrayList<char> hand3=new ArrayList<int>();
+  // private ArrayList<char> hand4=new ArrayList<int>();
+  // public Game(int p){
+  //   Players = p;
+  //   ArrayList<char> p =new ArrayList<char>();
+  //   p.add('X');
+  //   p.add('Z');
+  //   p.add('Q');
+  //   p.add('K');
+  //   p.add('J');
+  //   for(int i=0;i<2;i++){
+  //     p.add('Y');
+  //     p.add('W');
+  //     p.add('V');
+  //     p.add('P');
+  //     p.add('M');
+  //     p.add('H');
+  //     p.add('F');
+  //     p.add('C');
+  //     p.add('B');
+  //   }
+  //   for(int i=0;i<3;i++){
+  //     p.add('G');
+  //   }
+  //   for(int i=0;i<4;i++){
+  //     p.add('U');
+  //     p.add('S');
+  //     p.add('L');
+  //     p.add('D');
+  //   }
+  //   for(int i=0;i<2;i++){
+  //     p.add('Y');
+  //     p.add('W');
+  //     p.add('V');
+  //     p.add('P');
+  //     p.add('M');
+  //     p.add('H');
+  //     p.add('F');
+  //     p.add('C');
+  //     p.add('B');
+  //   }
+  //   for(int i=0;i<6;i++){
+  //     p.add('T');
+  //     p.add('R');
+  //     p.add('N');
+  //   }
+  //   for(int i=0;i<8;i++){
+  //     p.add('O');
+  //   }
+  //   for(int i=0;i<9;i++){
+  //     p.add('I');
+  //     p.add('A');
+  //   }
+  //   for(int i=0;i<12;i++){
+  //     p.add('E');
+  //   }
+  //   //RANDOMLY GENERATE PEICES FOR EACH HAND DEPENDING ON P
+  // }
   public void Start(){
     Board gameBoard= new Board();
     if(Players>4){
