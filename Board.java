e1d072ad0f511e6267d419d5235ce63f017eff12
Oliver Ripps <oripps@stuy.edu>
2019-01-08 23:04:42 -0500

    Board 'audited' and commented on

--- Board.java
+++ Board.java
@@ -1,19 +1,12 @@
 public class Board{
-  private int rows;
-  private int columns;
   private Pieces[][] board= new Pieces[15][15];
-  // Constructor for Board takes in an integer and 2D ArrayList of pieces
-  /*public Board(int r, int c, Pieces[][] a){
-    rows = r;
-    columns = c;
-    board = a;
-    start();
-  }*/
+
+  //Constructor of Board. Prompts set up board method
   public Board(){
-    rows=15;
-    columns=15;
     setupboard();
   }
+
+  //imputs coordinates and a char and changes the board to display that char
   public boolean modifyBoard(int xcor, int ycor,char newval){//will start from zero
     if(board[xcor][ycor].getPieceChar()!=' '){
       return false;
@@ -23,9 +16,13 @@ public class Board{
       return true;
     }
   }
+
+  //accesor method for piece at a coordinate
   public Pieces getPieces(int xcor, int ycor){
     return board[xcor][ycor];
   }
+
+  //creates a 15 by 15 2D array of Pieces with the correct multipliers
   public void setupboard(){
       for (int i = 0; i < 15; i++){
         for(int l = 0; l < 15; l++){
@@ -96,56 +93,23 @@ public class Board{
     }
 
 
-  /*public static void main(String args[]){
-    Pieces[][] a =new Pieces[15][15];
-    Pieces r = new Pieces(' ',0);//whatever letter you put in the '' will fill the whole board
-    for (int i = 0; i < a.length; i++){
-      for(int l = 0; l < a[i].length; l++){
-        a[i][l]=r;
-      }
-    }
-    Board test = new Board(8, 8, a);
-    System.out.println(test);
-  }*/
-  // Added clear method for a future restart method to be added in the game
-  /*public void clear(){
-    for(int i=0;i<board.length;i++){
-      for(int l=0;l<board[i].length;l++){
-        board[i][l]=null;
-    }
-  }
-}*/
-
-  // Accessor method for rows
-/*  public int getRows(){
-    return rows;
-  }
-  // Accessor method for columns
-  public int getColumns(){
-    return columns;
-  }
-  // Method to add pieces to array list in order to prepare to have pieces sorted randomly in ArrayList
-  /*public void addPiecesList(Pieces p){
-    pieces.add(p);
-  }*/
-  // Method that will initialize the 2D ArrayList and make it a board of pieces that are blank
-  /*public void addBlanks(){
-    for (int i = 0; i < rows; i++){
-      board.add(new ArrayList());
-      for (int a = 0; a < columns; a++){
-        board.get(i).add(Pieces(' ', 0, "white"));
-      }
-    }
-  }
   // toString method that will display board as a 2D ArrayList of pieces
+  //The format is char,value,letter*,word*,color
   public String toString(){
-    String str = "\n \n                       SCRABBLE BOARD\n";
+    String str = "\n \n                       SCRABBLE BOARD(format is char,value,letter*,word*,color)\n";
     str+= "-----------------------------------------------------------\n";
     for (int i = 0; i < board.length; i++){
       //str+="";
       for(int a = 0; a < board[i].length; a++){
-        str += ((board[i][a]).toString());
-        str+= " ";
+        str += ((board[i][a]).getPieceChar());
+        str += ",";
+        str += ((board[i][a]).getValue());
+        str += ",";
+        str += ((board[i][a]).getLetterMultiplier());
+        str += ",";
+        str += ((board[i][a]).getWordMultiplier());
+        str += ",";
+        str += ((board[i][a]).getColor());
       }
       if(i<board.length-1){
         str += "\n-----------------------------------------------------------\n";
@@ -153,5 +117,5 @@ public class Board{
     }
     str += "\n-----------------------------------------------------------";
     return str;
-  }*/
+  }
 }
