a87a62dd4f85b0f14bd1a7fa0f6a01d10319a50e
dramsammy <dramsammy@stuy.edu>
2019-01-09 22:14:35 -0500

    added some imports

--- Game.java
+++ Game.java
@@ -1,4 +1,19 @@
 import java.util.ArrayList;
+import java.util.*;
+import com.googlecode.lanterna.terminal.Terminal.SGR;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+import com.googlecode.lanterna.screen.Screen;
 public class Game {
   private int Players;
   private Board gameBoard;
@@ -99,5 +114,9 @@ public class Game {
       //str+= (returnString(i)).    FIGURE OUT HOW TO REFERENCE PLAYER TO STRING
     }
     return str;
+  }
+
+  public static void main(String[] args) {
+
   }
 }
