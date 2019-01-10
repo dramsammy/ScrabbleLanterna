054aefcced19d4ea1ac501114e6c9268f2f2f42f
dramsammy <dramsammy@stuy.edu>
2019-01-09 22:31:56 -0500

    fixed some coords

--- Game.java
+++ Game.java
@@ -17,7 +17,7 @@ import com.googlecode.lanterna.screen.Screen;
 public class Game {
   private int Players;
   private Board gameBoard;
-  private ArrayList<char> pile= new ArrayList<int>();
+  // private ArrayList<char> pile= new ArrayList<int>();
   // private ArrayList<char> hand1=new ArrayList<int>();
   // private ArrayList<char> hand2=new ArrayList<int>();
   // private ArrayList<char> hand3=new ArrayList<int>();
@@ -26,68 +26,68 @@ public class Game {
   //Game constructor
   //Starts by creating the pile of pieces
   //Creates each player and their hand
-  public Game(int play){
-    Players = play;
-    ArrayList<char> p =new ArrayList<char>();
-    p.add('X');
-    p.add('Z');
-    p.add('Q');
-    p.add('K');
-    p.add('J');
-    for(int i=0;i<2;i++){
-      p.add('Y');
-      p.add('W');
-      p.add('V');
-      p.add('P');
-      p.add('M');
-      p.add('H');
-      p.add('F');
-      p.add('C');
-      p.add('B');
-    }
-    for(int i=0;i<3;i++){
-      p.add('G');
-    }
-    for(int i=0;i<4;i++){
-      p.add('U');
-      p.add('S');
-      p.add('L');
-      p.add('D');
-    }
-    for(int i=0;i<2;i++){
-      p.add('Y');
-      p.add('W');
-      p.add('V');
-      p.add('P');
-      p.add('M');
-      p.add('H');
-      p.add('F');
-      p.add('C');
-      p.add('B');
-    }
-    for(int i=0;i<6;i++){
-      p.add('T');
-      p.add('R');
-      p.add('N');
-    }
-    for(int i=0;i<8;i++){
-      p.add('O');
-    }
-    for(int i=0;i<9;i++){
-      p.add('I');
-      p.add('A');
-    }
-    for(int i=0;i<12;i++){
-      p.add('E');
-    }
-    //BLANK TILES
-    pile=p;
-    for (int i = 0; i < Players; i++){
-      /*Player i = new Player();//ISSUE WITH NAMING IT
-      i.drawNewHand();*/
-    }
-    Board currentGame = new Board();
-}
+//   public Game(int play){
+//     Players = play;
+//     ArrayList<char> p =new ArrayList<char>();
+//     p.add('X');
+//     p.add('Z');
+//     p.add('Q');
+//     p.add('K');
+//     p.add('J');
+//     for(int i=0;i<2;i++){
+//       p.add('Y');
+//       p.add('W');
+//       p.add('V');
+//       p.add('P');
+//       p.add('M');
+//       p.add('H');
+//       p.add('F');
+//       p.add('C');
+//       p.add('B');
+//     }
+//     for(int i=0;i<3;i++){
+//       p.add('G');
+//     }
+//     for(int i=0;i<4;i++){
+//       p.add('U');
+//       p.add('S');
+//       p.add('L');
+//       p.add('D');
+//     }
+//     for(int i=0;i<2;i++){
+//       p.add('Y');
+//       p.add('W');
+//       p.add('V');
+//       p.add('P');
+//       p.add('M');
+//       p.add('H');
+//       p.add('F');
+//       p.add('C');
+//       p.add('B');
+//     }
+//     for(int i=0;i<6;i++){
+//       p.add('T');
+//       p.add('R');
+//       p.add('N');
+//     }
+//     for(int i=0;i<8;i++){
+//       p.add('O');
+//     }
+//     for(int i=0;i<9;i++){
+//       p.add('I');
+//       p.add('A');
+//     }
+//     for(int i=0;i<12;i++){
+//       p.add('E');
+//     }
+//     //BLANK TILES
+//     pile=p;
+//     for (int i = 0; i < Players; i++){
+//       /*Player i = new Player();//ISSUE WITH NAMING IT
+//       i.drawNewHand();*/
+//     }
+//     Board currentGame = new Board();
+// }
   //Temporary to get lanterna up and running
   public Game(){
     Board currentGame = new Board();
@@ -121,11 +121,12 @@ public class Game {
   }
 
   public static void main(String[] args) {
-    Game();
+    Game newGame = new Game();
     Screen screen = TerminalFacade.createScreen();
     boolean display = true;
     screen.startScreen();
-    screen.putString(0, 0, "Testing", Terminal.Color.WHITE, Terminal.Color.BLACK);
+    screen.putString(0, 40, "Testing", Terminal.Color.WHITE, Terminal.Color.BLACK);
     screen.refresh();
+
   }
 }
