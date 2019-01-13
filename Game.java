3ab15066b05a001e452e967cfdf9e83fe1a9cda7
Oliver Ripps <oripps@stuy.edu>
2019-01-13 18:55:14 -0500

    Added temporary player score on the side

--- Game.java
+++ Game.java
@@ -18,7 +18,7 @@ import com.googlecode.lanterna.screen.Screen;
 public class Game {
   private int Players;
   private Board gameBoard;
-  // private ArrayList<char> pile= new ArrayList<char>();
+  private ArrayList<Character> pile= new ArrayList<Character>();
   // private ArrayList<char> hand1=new ArrayList<int>();
   // private ArrayList<char> hand2=new ArrayList<int>();
   // private ArrayList<char> hand3=new ArrayList<int>();
@@ -27,71 +27,67 @@ public class Game {
   //Game constructor
   //Starts by creating the pile of pieces
   //Creates each player and their hand
-//   public Game(int play){
-//     Players = play;
-//     ArrayList<char> p =new ArrayList<char>();
-//     p.add('X');
-//     p.add('Z');
-//     p.add('Q');
-//     p.add('K');
-//     p.add('J');
-//     for(int i=0;i<2;i++){
-//       p.add('Y');
-//       p.add('W');
-//       p.add('V');
-//       p.add('P');
-//       p.add('M');
-//       p.add('H');
-//       p.add('F');
-//       p.add('C');
-//       p.add('B');
-//     }
-//     for(int i=0;i<3;i++){
-//       p.add('G');
-//     }
-//     for(int i=0;i<4;i++){
-//       p.add('U');
-//       p.add('S');
-//       p.add('L');
-//       p.add('D');
-//     }
-//     for(int i=0;i<2;i++){
-//       p.add('Y');
-//       p.add('W');
-//       p.add('V');
-//       p.add('P');
-//       p.add('M');
-//       p.add('H');
-//       p.add('F');
-//       p.add('C');
-//       p.add('B');
-//     }
-//     for(int i=0;i<6;i++){
-//       p.add('T');
-//       p.add('R');
-//       p.add('N');
-//     }
-//     for(int i=0;i<8;i++){
-//       p.add('O');
-//     }
-//     for(int i=0;i<9;i++){
-//       p.add('I');
-//       p.add('A');
-//     }
-//     for(int i=0;i<12;i++){
-//       p.add('E');
-//     }
-//     //BLANK TILES
-//     pile=p;
-//     for (int i = 0; i < Players; i++){
-//       /*Player i = new Player();//ISSUE WITH NAMING IT
-//       i.drawNewHand();*/
-//     }
-//     Board currentGame = new Board();
-// }
-  //Temporary to get lanterna up and running
-  public Game(){
+  public Game(int play){
     gameBoard = new Board();
+    Players = play;
+    ArrayList<Character> p =new ArrayList<Character>();
+    p.add('X');
+    p.add('Z');
+    p.add('Q');
+    p.add('K');
+    p.add('J');
+    for(int i=0;i<2;i++){
+      p.add('Y');
+      p.add('W');
+      p.add('V');
+      p.add('P');
+      p.add('M');
+      p.add('H');
+      p.add('F');
+      p.add('C');
+      p.add('B');
+    }
+    for(int i=0;i<3;i++){
+      p.add('G');
+    }
+    for(int i=0;i<4;i++){
+      p.add('U');
+      p.add('S');
+      p.add('L');
+      p.add('D');
+    }
+    for(int i=0;i<2;i++){
+      p.add('Y');
+      p.add('W');
+      p.add('V');
+      p.add('P');
+      p.add('M');
+      p.add('H');
+      p.add('F');
+      p.add('C');
+      p.add('B');
+    }
+    for(int i=0;i<6;i++){
+      p.add('T');
+      p.add('R');
+      p.add('N');
+    }
+    for(int i=0;i<8;i++){
+      p.add('O');
+    }
+    for(int i=0;i<9;i++){
+      p.add('I');
+      p.add('A');
+    }
+    for(int i=0;i<12;i++){
+      p.add('E');
+    }
+    //BLANK TILES
+    pile=p;
+    for (int i = 0; i < Players; i++){
+      /*Player i = new Player();//ISSUE WITH NAMING IT
+      i.drawNewHand();*/
+    }
   }
 
   //makes a name for each player
@@ -111,11 +107,12 @@ public class Game {
 }*/
 
   //creates a full scoreboard(temporarily just displays random stuff)
-   public String displyNames(){
-    String str = "";
+   public String displayNames(){
+    String str = "TEMPORARY PLACEHOLDER FOR REAL PLAYERS \n \nPoints!!! \n";
     for(int i=0; i< Players;i++){//may run into same name issue as constructor
-      str+= "Oliver \n";//TEMPORARY
-      str+= (i* 45);
+      str+= "Player" + (i+1) + ": ";//TEMPORARY
+      str+= (i+ 46);
+      str+="\n";
       //str+= (returnString(i)).    FIGURE OUT HOW TO REFERENCE PLAYER TO STRING
     }
     return str;
@@ -178,7 +175,7 @@ public static void putString(int r, int c, Terminal t, String s){
 
 
   public static void main(String[] args) {
-    Game newGame = new Game();
+    Game newGame = new Game(2);//replace with args
     Terminal screen = TerminalFacade.createTextTerminal();
     screen.enterPrivateMode();
     boolean display = true;
@@ -187,8 +184,9 @@ public static void putString(int r, int c, Terminal t, String s){
     while(display){
       Key key = screen.readInput();
       //Textbox t= new TextBox(12, "SCRABBLE 2.0™", SINGLE_LINE);
-      putString(33, 0, screen, "SCRABBLE 2.0™" );
-      putString(25, 2, screen, newGame.gameBoard.toString(), newGame.gameBoard);
+      putString(53, 0, screen, "SCRABBLE 2.0™" );
+      putString(45, 2, screen, newGame.gameBoard.toString(), newGame.gameBoard);
+      putString(0, 0, screen, newGame.displayNames());
 			screen.applyForegroundColor(Terminal.Color.BLACK);
       screen.moveCursor(0,0);
       if (key != null){
