8bebe920c93fcfddc5763a966e499455b94e355d
Oliver Ripps <oripps@stuy.edu>
2019-01-20 15:52:21 -0500

    testing scanning

--- Driver.java
+++ Driver.java
@@ -4,17 +4,21 @@ public class Driver{
     Pieces i = new Pieces();
     Pieces l = new Pieces();
     Pieces r = new Pieces();
-    i.setLetter('R');
-    l.setLetter('A');
-    r.setLetter('N');
-    Pieces[] g = new Pieces[3];
+    Pieces t = new Pieces();
+    i.setLetter('p');
+    l.setLetter('L');
+    r.setLetter('a');
+    t.setLetter('Y');
+    Pieces[] g = new Pieces[4];
     g[0] = i;
     g[1] = l;
     g[2] = r;
+    g[3] = t;
+    System.out.println("" +g[0].getPieceChar() +g[1].getPieceChar()+g[2].getPieceChar() + g[3].getPieceChar());
     System.out.println(b.isValidWord(g));
-    Player test = new Player(b, "hi");
+    /*Player test = new Player(b, "hi");
     System.out.println(test.theHandArrayList.get(0).handpiecetoString());
     System.out.println(i);
-    System.out.println(test.theHandArrayList.remove(test.theHand[0]));
+    System.out.println(test.theHandArrayList.remove(test.theHand[0]));*/
 }
 }
